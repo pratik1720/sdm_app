ec78ead7f34079b8e51ea4fde85abc3a2f68e28f
pratik1720 <pratikkhond345@gmail.com>
2024-01-04 14:44:48 +0530

    new app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const path=require("path")
+const app=express()
+
+const staticfolder=express.static(path.join(__dirname,"public"));
+app.use(staticfolder);
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname,"index.html"))
+
+})
+app.listen(3000,()=>{
+  console.log("server is listening on port 3000...")
+
+})
